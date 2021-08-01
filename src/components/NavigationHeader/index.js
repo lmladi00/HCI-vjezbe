f37e24107722266b70c57d0e7c12122ccf697b0d
lmladi00 <lmladi00@fesb.hr>
2021-08-02 00:19:33 +0200

    Adding NavigationHeader

---
+++ src/components/NavigationHeader/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import * as styles from './style.module.css'
+
+const NavigationHeader = () => (
+  <section className={styles.navigationHeader}>
+  </section>
+)
+export default NavigationHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import React from "react"
 
 import ContactBar from '../components/contactBar'
 
+import NavigationHeader from '../components/NavigationHeader'
 
 //ContactBar tag (moduralan) se mijenja sa kodom iz foldera u komponentama
 
 const IndexPage = () => (
   <main>
     <ContactBar /> 
+    <NavigationHeader />
   </main>
 )
 
