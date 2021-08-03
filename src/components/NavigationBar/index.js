e76d9639ee96c7191d4f1bc631625ee3b960cb8d
lmladi00 <lmladi00@fesb.hr>
2021-08-03 21:15:25 +0200

    adding NavigationBar part 1

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 import * as styles from './style.module.css'
-
-const NavigationBar = () => <section></section>
+//sadržava tabove
+//styles.active - na njemu smo trenutno i oznacen je po defaultu i nikoji drugi ne moze biti aktivan, to cemo posli rijesiti
+const NavigationBar = () => (
+    <nav className={styles.navigationBar}>
+        <li className={styles.active}>Home</li>
+        <li>Accommodation</li>
+        <li>Photo gallery</li>
+        <li>Contact</li>
+    </nav>
+  )  
 
 export default NavigationBar
