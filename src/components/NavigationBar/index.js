a114b4ddfc50a9068da098ac9e8fc35867c733b9
lmladi00 <lmladi00@fesb.hr>
2021-08-03 21:40:49 +0200

    refactor tabs to use map()

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import * as styles from './style.module.css'
 //sadržava tabove
-//styles.active - na njemu smo trenutno i oznacen je po defaultu i nikoji drugi ne moze biti aktivan, to cemo posli rijesiti
-const NavigationBar = () => (
+//styles.active - na njemu smo trenutno i oznacen je po defaultu i nikoji drugi ne moze biti aktivan, to cemo
+/*const NavigationBar = () => (
     <nav className={styles.navigationBar}>
         <li className={styles.active}>Home</li>
         <li>Accommodation</li>
@@ -11,5 +11,28 @@ const NavigationBar = () => (
         <li>Contact</li>
     </nav>
   )  
+
+export default NavigationBar
+*/
+//2. NACIN
+//RAZLIKA JE STO NEMAMO VISE HOME KAO ACTIVE!!!
+const navTabs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
+ //ako je home stavi aktiv ako nije onda bez!
+ //bolje if else bolje se vidi, umjesto ternarnog operatora!
+
+const NavigationBar = () => (
+  <nav className={styles.navigationBar}>
+    {navTabs.map(tab => {
+      if (tab === 'Home')
+      return (
+        <li className={styles.active}>
+          {tab}
+        </li>)
+      else
+        return <li>{tab}</li>
+      }
+    )}
+  </nav>
+)
 
 export default NavigationBar
