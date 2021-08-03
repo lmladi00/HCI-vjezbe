ea04c874bd7d8c3f11fa3b7da437efe7ae4328fb
lmladi00 <lmladi00@fesb.hr>
2021-08-03 20:51:16 +0200

    Adding Logo

--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+//dodali smo Logo u NavigationHeader da se prikaze na stranici
+import Logo from '../Logo'
 
 import * as styles from './style.module.css'
 
 const NavigationHeader = () => (
   <section className={styles.navigationHeader}>
+    <Logo />
   </section>
 )
 export default NavigationHeader
