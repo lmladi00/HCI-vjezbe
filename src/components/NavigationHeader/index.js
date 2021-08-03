6608f2f96363dd6fe95b45f60e183f2086512810
lmladi00 <lmladi00@fesb.hr>
2021-08-03 21:15:14 +0200

    adding NavigationBar part 1

--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 //dodali smo Logo u NavigationHeader da se prikaze na stranici
+//kad smo išli obrnuto, na kraju smo importali Navigation 
+//koji sadrži NavigationBar i Combobox
 import Logo from '../Logo'
 import Navigation from '../Navigation'
 
